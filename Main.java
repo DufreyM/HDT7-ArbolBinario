59fa097269143e1068499e1f3f7ae5eecf35dbab
Leonardo Mejía <leomejia646@gmail.com>
2024-03-19 19:20:09 -0600

    Creación de lector de texto y cambios para el main

--- Main.java
+++ Main.java
@@ -1,17 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
-        dictionary.buildDictionaryTree("diccionario.txt"); // Replace "diccionario.txt" with your file name
+
+        //DICCIONARIO
+        dictionary.buildDictionaryTree("diccionario.txt");
         BinaryTree<String, String> binaryTree = dictionary.getBinaryTree();
         List<Association<String, String>> sortedDictionary = binaryTree.inorderTraversal();
+        
         for (Association<String, String> association : sortedDictionary) {
             System.out.println("(" + association.key + ", " + association.value + ")");
         }
-
-        String translatedText = dictionary.translateText("The woman asked me to do my homework about my town.");
+        //Texto a traducir
+        String text = readTextFromFile("texto.txt");
+        
+        String translatedText = dictionary.translateText(text);
+        
+        System.out.println("Texto traducido:");
         System.out.println(translatedText);
     }
+    
+    private static String readTextFromFile(String fileName) {
+        StringBuilder text = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
 }
-
